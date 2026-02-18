ac0fcf36d714d10ac519f2fc810e60b434ca50b5
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2026-02-18 16:31:20 +0530

    create if user add manualy the food so it will chekc on data base then save the food into the db

--- controllers/FoodController.js
+++ controllers/FoodController.js
@@ -2,99 +2,207 @@ import Food from '../models/Food.js';
 import User from '../models/User.js';
 import mongoose from 'mongoose';
 import { configDotenv } from 'dotenv';
+import { findOrCreateFood } from '../services/foodItemServices/foodOrCreateFood.js';
 
 configDotenv()
-const postFood =  async (req, res) => {
+// const postFood =  async (req, res) => {
   
-  try {
-        const { userId } = req.user; // Extract userId from the request
-        console.log('this is userId', userId);
-        const { breakfast, lunch, dinner } = req.body;
-        const userExists = await User.findById(userId);
+//   try {
+//         const { userId } = req.user; // Extract userId from the request
+//         console.log('this is userId', userId);
+//         const { breakfast, lunch, dinner } = req.body;
+//         const userExists = await User.findById(userId);
 
-        if (!userExists) {
-          return res.status(404).json({ message: "User not found" });
-        }
+//         if (!userExists) {
+//           return res.status(404).json({ message: "User not found" });
+//         }
 
-        const currentDate = new Date();
-        const dateFormat = currentDate.toISOString().split('T')[0]; // Format: YYYY-MM-DD
+//         const currentDate = new Date();
+//         const dateFormat = currentDate.toISOString().split('T')[0]; // Format: YYYY-MM-DD
 
-        const startOfDay = new Date(dateFormat); // Midnight of the day
-        const endOfDay = new Date(dateFormat);
-        endOfDay.setDate(endOfDay.getDate() + 1); // Midnight of next day
+//         const startOfDay = new Date(dateFormat); // Midnight of the day
+//         const endOfDay = new Date(dateFormat);
+//         endOfDay.setDate(endOfDay.getDate() + 1); // Midnight of next day
 
-        let foodData = await Food.findOne({ user:userId,createdAt: { $gte: startOfDay,$lt: endOfDay}});
+//         let foodData = await Food.findOne({ user:userId,createdAt: { $gte: startOfDay,$lt: endOfDay}});
 
-        // console.log("foodData post : ", foodData)
+//         // console.log("foodData post : ", foodData)
 
-        const formatItems = (mealItems) => {
-          if (!mealItems) return [];
-          if (Array.isArray(mealItems)) {
-              return mealItems.map(item => ({ item, quantity: 1 }));
-          }
-          return [{ item: mealItems, quantity: 1 }];
-        };
+//         const formatItems = (mealItems) => {
+//           if (!mealItems) return [];
+//           if (Array.isArray(mealItems)) {
+//               return mealItems.map(item => ({ item, quantity: 1 }));
+//           }
+//           return [{ item: mealItems, quantity: 1 }];
+//         };
 
-        if (foodData) {
-            if (breakfast) {
-                const formattedBreakfast = formatItems(breakfast);
-                formattedBreakfast.forEach(newItem => {
-                    const existingItem = foodData.breakfast.find(item => item.item.toString() === newItem.item);
-                    if (existingItem) {
-                        existingItem.quantity += newItem.quantity;
-                    } else {
-                        foodData.breakfast.push(newItem);
-                    }
-                });
-            }
+//         if (foodData) {
+//             if (breakfast) {
+//                 const formattedBreakfast = formatItems(breakfast);
+//                 formattedBreakfast.forEach(newItem => {
+//                     const existingItem = foodData.breakfast.find(item => item.item.toString() === newItem.item);
+//                     if (existingItem) {
+//                         existingItem.quantity += newItem.quantity;
+//                     } else {
+//                         foodData.breakfast.push(newItem);
+//                     }
+//                 });
+//             }
 
-            if (lunch) {
+//             if (lunch) {
 
-              const formattedLunch = formatItems(lunch);
+//               const formattedLunch = formatItems(lunch);
 
-              formattedLunch.forEach(newItem => {
-                const existingItem = foodData.lunch.find(item => item.item.toString() === newItem.item);
-                if (existingItem) {
-                    existingItem.quantity += newItem.quantity;
-                } else {
-                    foodData.lunch.push(newItem);
-                }
-              });
+//               formattedLunch.forEach(newItem => {
+//                 const existingItem = foodData.lunch.find(item => item.item.toString() === newItem.item);
+//                 if (existingItem) {
+//                     existingItem.quantity += newItem.quantity;
+//                 } else {
+//                     foodData.lunch.push(newItem);
+//                 }
+//               });
               
-            }
+//             }
 
-            if (dinner) {
-                const formattedDinner = formatItems(dinner);
-                formattedDinner.forEach(newItem => {
-                    const existingItem = foodData.dinner.find(item => item.item.toString() === newItem.item);
-                    if (existingItem) {
-                        existingItem.quantity += newItem.quantity;
-                    } else {
-                        foodData.dinner.push(newItem);
-                    }
-                });
-            }
+//             if (dinner) {
+//                 const formattedDinner = formatItems(dinner);
+//                 formattedDinner.forEach(newItem => {
+//                     const existingItem = foodData.dinner.find(item => item.item.toString() === newItem.item);
+//                     if (existingItem) {
+//                         existingItem.quantity += newItem.quantity;
+//                     } else {
+//                         foodData.dinner.push(newItem);
+//                     }
+//                 });
+//             }
 
-            const updatedFood = await foodData.save();
-            console.log('this is updated Food', updatedFood);
+//             const updatedFood = await foodData.save();
+//             console.log('this is updated Food', updatedFood);
 
-            return res.status(200).json(updatedFood);
+//             return res.status(200).json(updatedFood);
 
-        } else {
-            const newFood = new Food({
-                user:userId,
-                breakfast: formatItems(breakfast),
-                lunch: formatItems(lunch),
-                dinner: formatItems(dinner)
-            });
+//         } else {
+//             const newFood = new Food({
+//                 user:userId,
+//                 breakfast: formatItems(breakfast),
+//                 lunch: formatItems(lunch),
+//                 dinner: formatItems(dinner)
+//             });
 
-            await newFood.save();
-            return res.status(201).json(newFood);
-        }
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+//             await newFood.save();
+//             return res.status(201).json(newFood);
+//         }
+//     } catch (error) {
+//         return res.status(500).json({ message: error.message });
+//     }
+// }
+
+const postFood = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { breakfast, lunch, dinner } = req.body;
+
+    // ----- 1. Check user -----
+    if (!mongoose.isValidObjectId(userId)) {
+      return res.status(400).json({ message: "Invalid user" });
     }
-}
+
+    const userExists = await User.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // ----- 2. Get today's IST date -----
+    const now = new Date();
+
+    const startOfDay = new Date(now);
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const endOfDay = new Date(now);
+    endOfDay.setHours(23, 59, 59, 999);
+
+    // ----- 3. Find today's food log -----
+    let foodData = await Food.findOne({
+      user: userId,
+      date: { $gte: startOfDay, $lte: endOfDay }
+    });
+
+    // ----- 4. Convert food names -> itemIds -----
+    const formatItems = async (mealItems) => {
+      if (!mealItems) return [];
+
+      const itemsArray = Array.isArray(mealItems) ? mealItems : [mealItems];
+      const formatted = [];
+
+      for (const foodName of itemsArray) {
+        const itemId = await findOrCreateFood(foodName);
+
+        formatted.push({
+          item: itemId,
+          quantity: 1
+        });
+      }
+
+      return formatted;
+    };
+
+    const formattedBreakfast = await formatItems(breakfast);
+    const formattedLunch = await formatItems(lunch);
+    const formattedDinner = await formatItems(dinner);
+
+    // ----- 5. If record exists → update -----
+    if (foodData) {
+
+      const mergeMeal = (existingMeal, newMeal) => {
+        newMeal.forEach(newItem => {
+          const existingItem = existingMeal.find(item =>
+            item.item.equals(newItem.item)
+          );
+
+          if (existingItem) {
+            existingItem.quantity += 1;
+          } else {
+            existingMeal.push(newItem);
+          }
+        });
+      };
+
+      if (formattedBreakfast.length)
+        mergeMeal(foodData.breakfast, formattedBreakfast);
+
+      if (formattedLunch.length)
+        mergeMeal(foodData.lunch, formattedLunch);
+
+      if (formattedDinner.length)
+        mergeMeal(foodData.dinner, formattedDinner);
+
+      await foodData.save();
+
+      return res.status(200).json({
+        message: "Food updated successfully",
+        food: foodData
+      });
+    }
+
+    // ----- 6. If no record → create -----
+    const newFood = await Food.create({
+      user: userId,
+      date: startOfDay,
+      breakfast: formattedBreakfast,
+      lunch: formattedLunch,
+      dinner: formattedDinner
+    });
+
+    return res.status(201).json({
+      message: "Food added successfully",
+      food: newFood
+    });
+
+  } catch (error) {
+    console.error("postFood Error:", error);
+    return res.status(500).json({ message: error.message });
+  }
+};
  
 const getFoodById =  async (req, res) => {
   
@@ -112,15 +220,14 @@ const getFoodById =  async (req, res) => {
         const startOfDay = new Date(dateFormat); // Midnight of the day
         const endOfDay = new Date(dateFormat);
         endOfDay.setDate(endOfDay.getDate() + 1); // Midnight of next day
-        
      
         const foodData = await Food.findOne({ user: id ,createdAt: { $gte: startOfDay,$lt: endOfDay}})
             .populate('user', '_id name mobile food')  
             .populate('breakfast.item')  
             .populate('lunch.item')     
-            .populate('dinner.item');    
+            .populate('dinner.item')
+            .lean();    
 
-            console.log('this  is foodData', foodData)
         if (!foodData) {
           const user = await User.findById(id, { _id: 1, name: 1, mobile: 1 , food: 1 });
             return res.status(200).json({ 
@@ -166,17 +273,15 @@ const getFoodById =  async (req, res) => {
             }
         });
 
-      
         const responseData = {
 
-          ...foodData._doc, 
+          ...foodData, 
           totalCalories,
           totalProtein : Math.round(totalProtein),
           totalCarbs : Math.round(totalCarbs),
           totalFat : Math.round(totalFat)
 
         };
-        console.log('thtis is response Data', responseData)
         return res.status(200).json(responseData);
     } catch (error) {
         console.error("Error fetching food data:", error);
