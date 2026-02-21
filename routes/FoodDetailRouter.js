b26b20cfee42cd247e211bb740bedba1b0987f08
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2026-02-21 10:35:05 +0530

    update yserDietDiaryScheam name of file is Food and update the foodContrller as well

--- routes/FoodDetailRouter.js
+++ routes/FoodDetailRouter.js
@@ -2,13 +2,13 @@ import express from 'express';
 const router = express.Router();
 
 import { Auth } from '../controllers/AuthController.js'
-import {postFood, getFoodById, updateFood, removeFood} from "../controllers/FoodController.js"
+import { getFoodById, updateFood, removeFood, addFoodToDiary} from "../controllers/FoodController.js"
 import { isNutriPlanValid } from '../middlewares/isNutriPlanValid.js';
 import { scanFood } from '../controllers/scanFoodController.js';
 import { uploadImage } from '../middlewares/uploadImage.js';
 
-router.post('/api/addFood',Auth,isNutriPlanValid,postFood);
-router.get('/api/getFoodById/:id',Auth,getFoodById);
+router.post('/api/addFood',Auth,isNutriPlanValid, addFoodToDiary);
+router.get('/api/getFoodById/:id',Auth, getFoodById);
 router.put('/api/updateFood', Auth,isNutriPlanValid, updateFood);
 router.delete('/api/removeFood',Auth,isNutriPlanValid, removeFood)
 
